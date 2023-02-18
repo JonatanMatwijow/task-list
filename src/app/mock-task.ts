429e4295c25ef64f58791fe55fbb4ddf4d264e8a
Jonatan-Matwijow <jonatanmatwijow@gmail.com>
2023-02-17 22:16:25 -0300

    Agregado de arrays con tareas

---
+++ src/app/mock-task.ts
@@ -0,0 +1,9 @@
+import { task } from "./task"
+export const TASK: task[] = [
+    {
+        id: 1,
+        text: "Tengo que ponerme las pilas para aprender bien y asi obtener laburo de desarrollador",
+        day: "Mayo 31 a las 22hs",
+        reminder: true
+    }
+]
